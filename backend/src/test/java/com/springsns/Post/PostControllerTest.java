2365076983ea6c084f5a3b393514e22732f1bfd2
zlarbals <zlarbals@gmail.com>
2021-12-19 02:03:29 +0900

    Get Post Test

--- backend/src/test/java/com/springsns/Post/PostControllerTest.java
+++ backend/src/test/java/com/springsns/Post/PostControllerTest.java
@@ -5,6 +5,8 @@ import com.springsns.account.AccountService;
 import com.springsns.account.SignInForm;
 import com.springsns.account.SignUpForm;
 import com.springsns.domain.Account;
+import com.springsns.domain.Post;
+import com.springsns.like.LikeService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +22,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Map;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -39,6 +40,12 @@ class PostControllerTest {
 
     @Autowired
     private AccountService accountService;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private LikeService likeService;
 
     private static final String ORIGINAL_FILE_NAME = "testpicture.PNG";
     private static final String PATH ="C:/Project/SpringSNS/backend/src/test/resources/testpicture.PNG";
@@ -68,8 +75,8 @@ class PostControllerTest {
     @DisplayName("사진 없는 게시글 등록 - 이메일 인증 안된 사용자")
     @Test
     void registerPostWithNoPictureWithUnauthenticatedUser() throws Exception{
-        String nickname = "unAuthenticate1";
-        String email = "unAuthenticate1@email.com";
+        String nickname = "postingUnAuthenticate1";
+        String email = "postingUnAuthenticate1@email.com";
         String password = "12345678";
         registerAccount(nickname,email,password);
 
@@ -83,8 +90,8 @@ class PostControllerTest {
     @DisplayName("사진 있는 게시글 등록 - 이메일 인증 안된 사용자")
     @Test
     void registerPostWithPictureWithUnauthenticatedUser() throws Exception{
-        String nickname = "unAuthenticate2";
-        String email = "unAuthenticate2@email.com";
+        String nickname = "postingUnAuthenticate2";
+        String email = "postingUnAuthenticate2@email.com";
         String password = "12345678";
         registerAccount(nickname,email,password);
 
@@ -106,8 +113,8 @@ class PostControllerTest {
     @DisplayName("사진 없는 게시글 등록 - 인증된 사용자")
     @Test
     void registerPostWithNoPictureWithAuthenticatedUser() throws Exception{
-        String nickname = "authenticate1";
-        String email = "authenticate1@email.com";
+        String nickname = "postingAuthenticate1";
+        String email = "postingAuthenticate1@email.com";
         String password = "12345678";
         registerAccount(nickname,email,password);
 
@@ -125,8 +132,8 @@ class PostControllerTest {
     @DisplayName("사진 있는 게시글 등록  - 인증된 사용자")
     @Test
     void registerPostWithPictureWithAuthenticatedUser() throws Exception{
-        String nickname = "authenticate2";
-        String email = "authenticate2@email.com";
+        String nickname = "postingAuthenticate2";
+        String email = "postingAuthenticate2@email.com";
         String password = "12345678";
         registerAccount(nickname,email,password);
 
@@ -145,6 +152,85 @@ class PostControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("existFile").value(true));
+    }
+
+    @DisplayName("게시글 가져오기(좋아요 표시) - 로그인한 사용자")
+    @Test
+    void getPostTestWithSignInUser() throws Exception{
+        //인증된 사용자 등록
+        String nickname = "getPostTest1";
+        String email = "getPostTest1@email.com";
+        String password ="12345678";
+        registerAccount(nickname,email,password);
+
+        authenticateEmail(email);
+
+        Object jwt = getJWTToken(email,password);
+
+        Account account = accountRepository.findByEmail(email);
+
+        //게시글을 생성
+        Post post=null;
+        for(int i=0;i<7;i++){
+            String content = "GetPostSignIn"+i;
+            post = Post.builder()
+                    .account(account)
+                    .content(content)
+                    .postFile(null)
+                    .build();
+
+            postRepository.save(post);
+        }
+
+        //마지막 게시글 좋아요 표시
+        likeService.addLike(email,post.getId());
+
+        //가장 마지막 게시글 = post
+        //가져온 첫 게시글이 post인지 확인
+        mockMvc.perform(get("/post").header("X-AUTH-TOKEN",jwt))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content.[0].content").value(post.getContent()))
+                .andExpect(jsonPath("$.content.[0].like").value(true));
+
+    }
+
+    @DisplayName("게시글 가져오기(좋아요 표시) - 로그아웃한 사용자")
+    @Test
+    void getPostTestWithSignOutUser() throws Exception{
+        //인증된 사용자 등록
+        String nickname = "getPostTest2";
+        String email = "getPostTest2@email.com";
+        String password ="12345678";
+        registerAccount(nickname,email,password);
+
+        authenticateEmail(email);
+
+        Account account = accountRepository.findByEmail(email);
+
+        //게시글을 생성
+        Post post=null;
+        for(int i=0;i<7;i++){
+            String content = "GetPostSignOut"+i;
+            post = Post.builder()
+                    .account(account)
+                    .content(content)
+                    .postFile(null)
+                    .build();
+
+            postRepository.save(post);
+        }
+
+        //마지막 게시글 좋아요 표시
+        likeService.addLike(email,post.getId());
+
+        //가장 마지막 게시글 = post
+        //가져온 첫 게시글이 post인지 확인
+        mockMvc.perform(get("/post"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content.[0].content").value(post.getContent()))
+                .andExpect(jsonPath("$.content.[0].like").value(false));
     }
 
     private void registerAccount(String nickname, String email, String password) {
